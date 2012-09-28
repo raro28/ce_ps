cb94dceb3ca4b4c0c682d3687afeb4c2dbf31a3f
3kthor3adward <3kthor3adward@gmail.com>
2012-09-28 02:43:50 -0500

    Correccion en HelperMethodsBase para soportar cambios en los parametros de los constructores.

--- Mx.Ipn.Esime.Statistics.BaseData.Tests/HelperMethodsBase.cs
+++ Mx.Ipn.Esime.Statistics.BaseData.Tests/HelperMethodsBase.cs
@@ -36,13 +36,13 @@ namespace Mx.Ipn.Esime.Statistics.BaseData.Tests
 				var cacheArgs = args.ToList ();
 				cacheArgs.Insert (0, data);
 
-				var ctor = typeof(TInquirer).GetConstructors ()
+				var ctors = typeof(TInquirer).GetConstructors ()
 					.Where (c 
 					        => c.GetParameters ()
 					        .Select (p => p.ParameterType)
-					        .SequenceEqual (cacheArgs.Select (arg => arg != null ? arg.GetType () : typeof(List<double>))))
-					.First ();
+					        .TypeSequenceIsAssignableFrom (cacheArgs.Select (arg => arg))).ToList ();
 
+				var ctor = ctors.First ();
 				var calculator = (TInquirer)ctor.Invoke (cacheArgs.ToArray ());
 				data.Sort ();
 
@@ -55,5 +55,31 @@ namespace Mx.Ipn.Esime.Statistics.BaseData.Tests
 		public abstract double CalcNthXile (IList<double> data, int xile, int nTh);
 		
 		public abstract double SampleMean (List<double> data);
+	}
+
+	internal static class SequenceCastExtension
+	{
+		public static bool TypeSequenceIsAssignableFrom (this IEnumerable<Type> source, IEnumerable<Object> sequence)
+		{
+			var canCast = true;
+
+			if (source.Count () == sequence.Count ()) {
+				if (source.Count () > 0) {
+					var srcEnum = source.GetEnumerator ();
+					var seqEnum = sequence.GetEnumerator ();
+					do {
+						if (seqEnum.Current == null) {
+							continue;
+						}
+
+						canCast = srcEnum.Current.IsAssignableFrom (seqEnum.Current.GetType ());
+					} while(srcEnum.MoveNext() && seqEnum.MoveNext() && canCast);
+				}
+			} else {
+				canCast = false;
+			}
+
+			return canCast;
+		}
 	}
 }

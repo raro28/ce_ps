5cc59af18a7d2fee4fadfd128cb127c3fb054fd5
3kthor3adward <3kthor3adward@gmail.com>
2012-10-09 17:31:40 -0500

    StatisticsInquirerBase

---
+++ Mx.Ipn.Esime.Statistics.Core/Base/SequenceCastExtension.cs
@@ -0,0 +1,45 @@
+namespace Mx.Ipn.Esime.Statistics.Core.Base
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    internal static class SequenceCastExtension
+    {
+        public static bool CanAssignValueSequenceI(this MethodInfo method, IEnumerable<object> args)
+        {
+            return method.GetParameters().Select(parameter => parameter.ParameterType).TypeSequenceIsAssignableFrom(args);
+        }
+
+        public static bool TypeSequenceIsAssignableFrom(this IEnumerable<Type> source, IEnumerable<object> sequence)
+        {
+            var canCast = true;
+            
+            if (source.Count() == sequence.Count())
+            {
+                if (source.Count() > 0)
+                {
+                    var srcEnum = source.GetEnumerator();
+                    var seqEnum = sequence.GetEnumerator();
+                    do
+                    {
+                        if (seqEnum.Current == null)
+                        {
+                            continue;
+                        }
+                        
+                        canCast = srcEnum.Current.IsAssignableFrom(seqEnum.Current.GetType());
+                    }
+                    while (srcEnum.MoveNext() && seqEnum.MoveNext() && canCast);
+                }
+            }
+            else
+            {
+                canCast = false;
+            }
+            
+            return canCast;
+        }
+    }
+}

181b772a34a79a61c66b31b78a544451125bd116
3kthor3adward <3kthor3adward@gmail.com>
2012-10-16 11:26:25 -0500

    Se reemplaza jqplot por highcharts

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -7,16 +7,22 @@ using Ninject;
 using Mx.Ipn.Esime.Statistics.Core.Base;
 using Mx.Ipn.Esime.Statistics.GroupedData;
 using Mx.Ipn.Esime.Statistics.UngroupedData;
+using DotNet.Highcharts;
+using DotNet.Highcharts.Options;
+using DotNet.Highcharts.Helpers;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly static Random rnd;
+
+        private readonly static StandardKernel kernel;
         
         static HomeController()
         {
             rnd = new Random(DateTime.Now.Millisecond);
+            kernel = new StandardKernel();
         }
         
         public static IEnumerable<double> GetRandomDataSample(int size)
@@ -38,16 +44,15 @@ namespace Web.Controllers
         {
             ViewBag.Title = "Análisis";
             var values = data.Split(',').Select(number => Double.Parse(number)).ToList();
-            var kernel = new StandardKernel();
-            kernel.Bind<GroupedXileInquirer>().ToSelf().InSingletonScope();
-            kernel.Bind<GroupedCentralTendecyInquirer>().ToSelf().InSingletonScope();
-            kernel.Bind<GroupedDispersionInquirer>().ToSelf().InSingletonScope();
-            kernel.Bind<DataDistributionFrequencyInquirer>().ToSelf().InSingletonScope();
-            kernel.Bind<UngroupedXileInquirer>().ToSelf().InSingletonScope();
-            kernel.Bind<UngroupedCentralTendecyInquirer>().ToSelf().InSingletonScope();
-            kernel.Bind<UngroupedDispersionInquirer>().ToSelf().InSingletonScope();
-            kernel.Bind<StatisticsInquirerBase>().To<UngroupedStatisticsInquirer>().InSingletonScope().Named("ungrouped");
-            kernel.Bind<StatisticsInquirerBase>().To<GroupedStatisticsInquirer>().InSingletonScope().Named("grouped");
+            kernel.Rebind<GroupedXileInquirer>().ToSelf().InSingletonScope();
+            kernel.Rebind<GroupedCentralTendecyInquirer>().ToSelf().InSingletonScope();
+            kernel.Rebind<GroupedDispersionInquirer>().ToSelf().InSingletonScope();
+            kernel.Rebind<DataDistributionFrequencyInquirer>().ToSelf().InSingletonScope();
+            kernel.Rebind<UngroupedXileInquirer>().ToSelf().InSingletonScope();
+            kernel.Rebind<UngroupedCentralTendecyInquirer>().ToSelf().InSingletonScope();
+            kernel.Rebind<UngroupedDispersionInquirer>().ToSelf().InSingletonScope();
+            kernel.Rebind<StatisticsInquirerBase>().To<UngroupedStatisticsInquirer>().InSingletonScope().Named("ungrouped");
+            kernel.Rebind<StatisticsInquirerBase>().To<GroupedStatisticsInquirer>().InSingletonScope().Named("grouped");
             kernel.Rebind<DataContainer>().ToMethod(context => new DataContainer(values)).InSingletonScope();
 
             ViewBag.Data = data;
@@ -58,15 +63,35 @@ namespace Web.Controllers
         }
 
         [ChildActionOnly]
-        public PartialViewResult GroupedReport(GroupedStatisticsInquirer model)
+        public PartialViewResult Histogram()
         {
-            return PartialView(model);
+            Highcharts chart = new Highcharts("histogram")
+                .SetXAxis(new XAxis
+                          {
+                    Categories = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }
+                })
+                    .SetSeries(new Series
+                               {
+                        Data = new Data(new object[] { 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4 })
+                    });
+            
+            return PartialView("HighChart", chart);
         }
 
         [ChildActionOnly]
-        public PartialViewResult UngroupedReport(UngroupedDispersionInquirer model)
+        public PartialViewResult Ogive()
         {
-            return PartialView(model);
+            Highcharts chart = new Highcharts("ogive")
+                .SetXAxis(new XAxis
+                          {
+                    Categories = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }
+                })
+                    .SetSeries(new Series
+                               {
+                        Data = new Data(new object[] { 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4 })
+                    });
+            
+            return PartialView("HighChart", chart);
         }
     }
 }

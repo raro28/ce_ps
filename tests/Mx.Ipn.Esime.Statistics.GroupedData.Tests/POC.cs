ea34a580663334f2b6e02a28401500e347de1e16
3kthor3adward <3kthor3adward@gmail.com>
2012-10-21 18:20:53 -0500

    licencia GPL

--- tests/Mx.Ipn.Esime.Statistics.GroupedData.Tests/POC.cs
+++ tests/Mx.Ipn.Esime.Statistics.GroupedData.Tests/POC.cs
@@ -1,9 +1,27 @@
-using NUnit.Framework;
-using Mx.Ipn.Esime.Statistics.Core.Base;
-using Ninject;
-
+/*
+ * Copyright (C) 2012 Hector Eduardo Diaz Campos
+ * 
+ * This file is part of Mx.DotNet.Statistics.
+ * 
+ * Mx.DotNet.Statistics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * Mx.DotNet.Statistics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Mx.DotNet.Statistics.  If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace Mx.Ipn.Esime.Statistics.GroupedData.Tests
 {
+    using NUnit.Framework;
+    using Mx.Ipn.Esime.Statistics.Core.Base;
+    using Ninject;
+
     [TestFixture()]
     public class POC
     {
